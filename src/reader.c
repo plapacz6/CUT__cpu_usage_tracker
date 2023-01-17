c65bb5b066fe80308b9b398a3bcdaa9025a7a909
pawel <plapacz6@gmail.com>
2023-01-17 17:33:18 +0100

    reader: passing msg in add_msg_to_rb_ra: correct

--- src/reader.c
+++ src/reader.c
@@ -106,7 +106,7 @@ int fillin_buff_M_tmpfile(char buff_M[], size_t buff_M_size, char *fname){
     exit(1);
   }
   size_t readed = fread(buff_M, 1, buff_M_size, fin );
-  if(readed < buff_M_size - 2){
+  if(readed < buff_M_size - 1){
     fprintf(stderr, "%s\n", "probably error during reading temporary file");
     fprintf(stderr, "reded [%lu] elements", readed);
   }
@@ -141,7 +141,7 @@ size_t calc_cpuN(char *buff_M, size_t buf_size){
  */
 void read_BM(char *buff_M, size_t buff_M_size, FILE *fp){
   /* read probably whole content from /proc/stat */
-  fread(buff_M, 1, buff_M_size - 2, fp);
+  fread(buff_M, 1, buff_M_size - 1, fp);
   buff_M[buff_M_size - 1] = '\0';
             // printf("\n%s\n%s\n%s\n%s\n",
             //   "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
@@ -169,7 +169,7 @@ void read_One_set(char buff_M[], size_t buff_M_size, char* msg_array, size_t cpu
   char *begin2 = NULL;  
   begin1 = &buff_M[0];  
   int core = 0;
-            printf("\n cpuN: %lu\n", cpuN);
+            // printf("\n cpuN: %lu\n", cpuN);
             // buff_M[buff_M_size - 1] = '\0';
             // printf("\n buff_M:\n%s\n", &buff_M[0]);
   
@@ -186,10 +186,10 @@ void read_One_set(char buff_M[], size_t buff_M_size, char* msg_array, size_t cpu
     // printf("sizeof(char):   %lu\n", sizeof(char));
     // printf("\n%%%%%%%%%%%%%%%%\n: ", "");
     char* msg = (char*) msg_array;
-    strncpy( (msg + (core * msg_size)), begin2, msg_size - 2);    
+    strncpy( (msg + (core * msg_size)), begin2, msg_size - 1);    
     *((msg + (core * msg_size)) + msg_size - 1) = '\0';
 
-            printf("msg:\n%s\n", (msg + (core * msg_size)));
+            //printf("msg:\n%s\n", (msg + (core * msg_size)));
 
     core++;
     begin1 = begin2 + 1;
@@ -233,16 +233,28 @@ int add_msg_to_rb_ra(char *msg_a, size_t msg_a_size, ring_buffer_T *prb){
 
     //1.pobierz hak
     char *ptr_cell = (char*) rb_get_back_hook(prb);
-    printf("%s\n", "4.4 hook for mgs obtained"); fflush(stdout);
+    //printf("%s\n", "4.4 hook for mgs obtained"); fflush(stdout);
     //2.skopiuj  
-    strncpy(ptr_cell, msg_a, msg_a_size);  
-    printf("%s\n", "4.5 message saved in ring_buffer"); fflush(stdout);
+    memcpy(ptr_cell, msg_a, msg_a_size);  
+    //printf("%s\n", "4.5 message saved in ring_buffer"); fflush(stdout);
 
   return 0;
 }
 
 char* get_msg_from_rb_ra(ring_buffer_T *prb){
   return (char*)rb_get_front_hook(prb);
+}
+
+void open_proc_stat_file(){
+  proc_stat_file = fopen("/proc/stat","r");
+  if(!proc_stat_file){
+    //thread_exit(1);
+    exit(1);
+  }  
+}
+void close_proc_stat_file(){
+  if(proc_stat_file != NULL) fclose(proc_stat_file);  
+  proc_stat_file = NULL;
 }
 /***************************************************/
 
@@ -251,8 +263,8 @@ void* reader(void *watchdog_tbl){
   size_t cpu_CorN = 0;
   char *buff_M = NULL;
   
-  printf("\n\n%s\n", "1. reader starting ...");
-  printf("1. buffer_M:---------------\n %s\n____________________________\n", buff_M);
+  // printf("\n\n%s\n", "1. reader starting ...");
+  // printf("1. buffer_M:---------------\n %s\n____________________________\n", buff_M);
 
 
   /* ----------   buffer_M   -----------------*/
@@ -260,56 +272,57 @@ void* reader(void *watchdog_tbl){
   char fname[strlen(fmt) + 16];
   sprintf(fname, fmt, rand());  
 
-  printf("\n1.2 tmpfile fname ready: %s\n", fname); fflush(stdout);
+  // printf("\n1.2 tmpfile fname ready: %s\n", fname); fflush(stdout);
 
   cat_proc_stat_file(fname);
 
-  printf("\n\n%s\n", "1.3 tmpfile ready");fflush(stdout);
+  // printf("\n\n%s\n", "1.3 tmpfile ready");fflush(stdout);
 
   buff_M_size = determine_buff_M_size(fname);
 
-  printf("\n\n%s\n", "1.4 buff_M size  - obtained");fflush(stdout); 
+  // printf("\n\n%s\n", "1.4 buff_M size  - obtained");fflush(stdout); 
 
   buff_M_size += 2;
   buff_M = create_buff_M(buff_M_size);
 
-  printf("\n\n%s\n", "1.5 buff_M created");fflush(stdout); 
+  // printf("\n\n%s\n", "1.5 buff_M created");fflush(stdout); 
 
   fillin_buff_M_tmpfile(&buff_M[0], buff_M_size, fname);
   
 
-  printf("\n\n%s\n", "1.6 tmpfile reader to buff_M");fflush(stdout); 
+  // printf("\n\n%s\n", "1.6 tmpfile reader to buff_M");fflush(stdout); 
 
   cpu_CorN = calc_cpuN(&buff_M[0], buff_M_size);
 
-  printf("\n\n%s\n", "1.7 number of cpu cors calculated");fflush(stdout); 
+  // printf("\n\n%s\n", "1.7 number of cpu cors calculated");fflush(stdout); 
 
   cpu_cors_N(cpu_CorN, 1);  
 
-  printf("\n\n%s\n", "1.8 number of cpu cors set");fflush(stdout); 
+  // printf("\n\n%s\n", "1.8 number of cpu cors set");fflush(stdout); 
 
   if(0 != remove(fname)){
     fprintf(stderr, "%s %s\n", "can't delete temporary file :", fname);
   }
-  printf("\n\n%s\n", "1.9 tmpfile - deleted ");fflush(stdout); 
+  // printf("\n\n%s\n", "1.9 tmpfile - deleted ");fflush(stdout); 
 
 
-  printf("%s\n", "2. buffer_M ready, preparing data table and ring_buff");
-  printf("2. buffer_M:------------\n %s\n____________________________\n", buff_M);
+  // printf("%s\n", "2. buffer_M ready, preparing data table and ring_buff");
+  // printf("2. buffer_M:------------\n %s\n____________________________\n", buff_M);
 
+  
   /* ------  data_table + ring_buffer   --------*/
 
 
   /* create data table for */
   
-  printf("%s\n", "2.1. setting message array size");
+  // printf("%s\n", "2.1. setting message array size");
 
   #define MSG_SIZE (128)
   size_t msg_size = MSG_SIZE;
   get_msg_size(MSG_SIZE, 1);
   #undef MSG_SIZE
 
-  printf("%s\n", "2.2. creating message array");
+  // printf("%s\n", "2.2. creating message array");
 
   G_msg_array = create_msg_array(cpu_CorN, msg_size);
 
@@ -317,31 +330,30 @@ void* reader(void *watchdog_tbl){
   rb_ra_data_table = create_rb_ra_data_table(10 * msg_size * cpu_CorN);
   rb_ra = rb_create(rb_ra_data_table, msg_size * cpu_CorN, 10);
 
-  printf("%s\n", "3. openinig /proc/stat");
+  // printf("%s\n", "3. openinig /proc/stat");
+
 
   /* ----------   /proc/stat   -----------------*/
-  proc_stat_file = fopen("/proc/stat","r");
-  if(!proc_stat_file){
-    //thread_exit(1);
-    exit(1);
-  }
 
+
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
   //signal for main to run analyzer thread
   //cnd_signal();
 
-  printf("%s\n", "4. main loop");
-
+  // printf("%s\n", "4. main loop");
+  
   //while(1){
   for(int k = 0; k < 10; k++){
-    printf("%s\n", "4.1 enter"); fflush(stdout);
+    open_proc_stat_file();  
+    // printf("%s\n", "4.1 enter"); fflush(stdout);
     
     read_BM(buff_M, buff_M_size, proc_stat_file);
 
-    printf("%s\n", "4.2 content /proc/stat readed"); fflush(stdout);
+    // printf("%s\n", "4.2 content /proc/stat readed"); fflush(stdout);
 
     read_One_set(&buff_M[0], buff_M_size, G_msg_array, cpu_CorN, msg_size);    
 
-    printf("%s\n", "4.3 messages for analyzer ready"); fflush(stdout);
+    // printf("%s\n", "4.3 messages for analyzer ready"); fflush(stdout);
 
     /*  podepnij do ring buffera */
     add_msg_to_rb_ra(G_msg_array, msg_size * cpu_CorN, rb_ra);
@@ -350,24 +362,22 @@ void* reader(void *watchdog_tbl){
     add_msg_to_rb_ra(G_msg_array, msg_size * cpu_CorN, rb_ra);
     char *msg_aa = get_msg_from_rb_ra(rb_ra);
     for(int i = 0; i < cpu_CorN; i++){
-      printf("\nmsg:%s\n", msg_aa + (i * msg_size)); 
+      printf("\nmsg %d:%s\n", i, (msg_aa + (i * msg_size))); 
     }
 
     //3.nanosleep()
     sleep(1);
 
-    printf("%s\n", "4.6 after sleep - ready for new /proc/stat read"); fflush(stdout);
+    // printf("%s\n", "4.6 after sleep - ready for new /proc/stat read"); fflush(stdout);
     /* -----------------------------*/
-    
-  
-
+    close_proc_stat_file();
   }//while
   printf("%s\n", "5. after main loop"); fflush(stdout); 
   
   //SIGTERM handler:
   destroy_msg_array(G_msg_array);
   destroy_buff_M(buff_M);
-  if(proc_stat_file) fclose(proc_stat_file);   //SIGTERM
+  if(proc_stat_file != NULL) fclose(proc_stat_file);   //SIGTERM
   rb_destroy(rb_ra);
   destroy_rb_ra_data_table(rb_ra_data_table);
   pthread_exit(0);
